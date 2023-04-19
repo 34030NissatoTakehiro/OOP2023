2ed126cdc49ee2adceea0f11f02dedceaf1f63ec
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-04-19 10:10:19 +0900

    新規作成

---
+++ Sample/Ballapp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ballapp {
+    class Program : Form {
+
+        private Timer moveTimer;
+        private SoccerBall soccerBall ;
+        private PictureBox pb;
+
+        static void Main(string[] args) {
+            Application.Run(new Program());
+        }
+        public Program() {
+            
+            this.Size = new Size(800, 600);
+            this.BackColor = Color.Red;
+            this.Text = "Ball Game";
+            //ボールインスタンス作成
+
+            soccerBall = new SoccerBall();
+            pb = new PictureBox();
+            pb.Image = soccerBall.Image;
+            
+            pb.Size = new Size(50, 50);//画像の表示サイズ
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;//画像の表示サイズ
+
+            moveTimer = new Timer();
+            moveTimer.Interval = 10;//タイマーのインターバル
+            moveTimer.Start();//タイマースタート
+            moveTimer.Tick += MoveTimer_Tick;
+
+
+            pb.Parent = this;
+
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e) {
+            soccerBall.Move();//移動
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);//画像の位置
+        }
+    }
+}
---
+++ Sample/Ballapp/SoccerBall.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ballapp {
+    class SoccerBall {
+        //  フィールド
+        private Image image;//画像データ
+
+        private double posX;//x座標
+        private double posY;//y座標
+        private double moveX = 5;//移動量
+        private double moveY = 5;//移動量
+
+        //コンストラクタ
+        public SoccerBall() {
+            Image = Image.FromFile(@"pic\Soccer_Ball.png");
+            posX = 0.0;
+            posY = 0.0;
+        }
+        //プロパティ
+        public double PosX { get => posX; set => posX = value; }
+        public double PosY { get => posY; set => posY = value; }
+        public Image Image { get => image; set => image = value; }
+
+        //メソット
+        public void Move() {
+            if (posY > 550 ||posY <0)
+            {
+                moveY = -moveY;
+                
+            }
+            if (posX > 750 || posX < 0)
+            {
+                moveX = -moveX;
+
+            }
+
+
+
+
+            posX = posX + moveX;
+            posY = posY + moveY;
+        }
+    }
+}
