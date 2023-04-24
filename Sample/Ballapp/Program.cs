15e535c678a56e3bf451f0175524fbf30ecd73d5
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-04-24 10:09:49 +0900

    ballcount を追加

--- Sample/Ballapp/Program.cs
+++ Sample/Ballapp/Program.cs
@@ -14,7 +14,7 @@ namespace Ballapp {
         private PictureBox pb;
         private List<SoccerBall> balls = new List<SoccerBall>();//ボール格納
         private List<PictureBox> pbs = new List<PictureBox>();//表示用
-
+        private int ballcount = 0;
 
         static void Main(string[] args) {
             Application.Run(new Program());
@@ -23,9 +23,8 @@ namespace Ballapp {
             
             this.Size = new Size(800, 600);
             this.BackColor = Color.Red;
-            this.Text = "Ball Game";
+           
             this.MouseClick += Program_MouseClick;
-            //ボールインスタンス作成
 
             
 
@@ -37,23 +36,24 @@ namespace Ballapp {
 
         private void Program_MouseClick(object sender, MouseEventArgs e) {
 
+            this.Text = "Ball Game" + "(" + (ballcount + 1) + ")";
             soccerBall = new SoccerBall(e.X -25,e.Y - 25);
             pb = new PictureBox();
             pb.Image = soccerBall.Image;
-
             pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);//画像の位置
             pb.Size = new Size(50, 50);//画像の表示サイズ
             pb.SizeMode = PictureBoxSizeMode.StretchImage;//画像の表示サイズ
             pb.Parent = this;
             balls.Add(soccerBall);
             pbs.Add(pb);
-
+            ballcount += 1;
             moveTimer.Start();//タイマースタート
         }
 
         private void MoveTimer_Tick(object sender, EventArgs e) {
             for (int i = 0; i < balls.Count; i++)
             {
+                
                 balls[i].Move();//移動
                 pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);//画像の位置
             }
