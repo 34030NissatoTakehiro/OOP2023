474cd58416ac3f8a6324acb930d9fe2fbc7dfeb4
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-05-08 11:47:19 +0900

    10日前出力

--- chapter01/Product/Program.cs
+++ chapter01/Product/Program.cs
@@ -9,11 +9,29 @@ namespace ProductSample {
     
     class Program {
         static void Main(string[] args) {
-            Product karinto = new Product(123, "かりんとう", 180);
-            Product daihuku  = new Product(235, "大福", 160);
+            #region P26のサンプル
+            //Product karinto = new Product(123, "かりんとう", 180);
+            //Product daihuku  = new Product(235, "大福", 160);
 
-            Console.WriteLine("税込価格："+karinto.GetPriceIncludingTax());
-            Console.WriteLine("税込価格：" + daihuku.GetPriceIncludingTax());
+            //Console.WriteLine("税込価格："+karinto.GetPriceIncludingTax());
+            //Console.WriteLine("税込価格：" + daihuku.GetPriceIncludingTax());
+            #endregion
+
+            DateTime date = new DateTime(2023, 5, 8);
+           
+
+            string result = date.ToString("今日は"+"yyyy年MM月dd日");
+            Console.WriteLine(result);
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+            DateTime daysbefor10 = date.AddDays(-10);
+
+            
+
+            Console.WriteLine("今日の１０日後は" + daysAfter10.Month+"月" + daysAfter10.Day + "日です");
+            Console.WriteLine("今日の１０日前は"+ daysbefor10.Month + "月" + daysbefor10.Day + "日です");
+
 
 
         }
