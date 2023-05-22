20b8c5518db5793774b9f8bddb841a0244a7da1d
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-05-22 09:30:20 +0900

    p79問題2,2完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    //フィートとメートル単位変換クラス
+    public   class InchConverter {
+        private const double retio = 0.0254;
+
+        //インチからフィートを求める
+        public static double FromMeter(double meter) {
+            return meter / retio;
+        }
+        //インチからメートルを求める
+        public static double ToMeter(double inch) {
+            return inch * retio;
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length < 3 && args[0] == "-tom") {
+                PrintInchToMeterList(int.Parse(args[1]),int.Parse(args[2]));
+            } else if (args.Length < 3 && args[0] == "-toi") {
+                PrintMeterToInchList(int.Parse(args[1]), int.Parse(args[2]));
+            }
+        }
+
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.FromMeter(inch);
+                Console.WriteLine("{0} inch ={1:0.0000}m", inch, meter);
+
+            }
+        }
+
+        private static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.ToMeter(meter);
+                Console.WriteLine("{0}m={1:0.0000}inch", meter, inch);
+
+            }
+        }
+    }
+}
