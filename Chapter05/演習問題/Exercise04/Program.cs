d77ebc0434ecefc18ba0f53d7da6096389d38135
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-06-06 09:44:11 +0900

    p159演習問題5,4

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define Nonarray
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +9,16 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+
+            var count = line.Replace("Novelist=", "作家：").Replace("BestWork=", "代表作:").Replace("Born=", "誕生日：");
+            var split = count.Split(';');
+            foreach (var s in split) {
+                Console.WriteLine(s);
+            }
+
+            
         }
     }
 }
