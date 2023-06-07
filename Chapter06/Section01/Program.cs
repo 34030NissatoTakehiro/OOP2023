c5478f10ef1ef0ba345c97ce1bb23e56c156fdcd
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-06-07 09:59:44 +0900

    オブジェクトLINQ

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+
+            var numbers = new List<int> { 9,7,5,4,2,5,4,0,4,1,0,4,};
+            var books =Books. GetBooks();
+
+            Console.WriteLine(numbers.Average());
+            var mmm = books.Where(x => x.Title.Contains("物語")).Average(x => x.Pages);
+            var title = books.OrderByDescending(X => X.Title.Length);
+            Console.WriteLine("物語が入っている書籍の平均ページ数：" + mmm + "ページ");
+            foreach (var item in title) {
+                Console.WriteLine(item.Title);
+            }
+        }
+
+
+    }
+}
