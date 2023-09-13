21852753a7f5c15ca3966b179239f727957de0dd
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-09-13 11:10:18 +0900

    データ注釈

--- Chapter13/SampleEntityFrameWork/Models/Auther.cs
+++ Chapter13/SampleEntityFrameWork/Models/Auther.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleEntityFrameWork.Models {
    public class Auther {
         public int Id { get; set; }
+        [MaxLength(30)]
+        [Required]
         public string Name { get; set; }
         public DateTime birthday { get; set; }
         public string Gender { get; set; }
--- Chapter13/SampleEntityFrameWork/Models/Book.cs
+++ Chapter13/SampleEntityFrameWork/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ using System.Threading.Tasks;
 namespace SampleEntityFrameWork.Models {
    public class Book {
         public int Id { get; set; }  //主キー
+
+        [Required]
+
         public string Title { get; set; }
         public int PublishedYear { get; set; }
         public virtual Auther Author{ get; set; }
