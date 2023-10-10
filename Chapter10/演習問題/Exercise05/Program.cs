2082725992099e7def27f7740c0bc2889dcf386a
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-10-10 11:53:21 +0900

    Exercise6  10-2まで

--- Chapter10/演習問題/Exercise05/Program.cs
+++ Chapter10/演習問題/Exercise05/Program.cs
@@ -10,23 +10,29 @@ namespace Exercise05 {
     class Program {
         static void Main(string[] args) {
             TagLower("sample.html");
+
+            // これ以降は確認用
+            var text = File.ReadAllText("sample.html");
+            Console.WriteLine(text);
         }
 
         private static void TagLower(string file) {
             var lines = File.ReadLines(file);
             var sb = new StringBuilder();
-            var patton = @"<(\w[^>]+)>";
-           
-
-
-
-
-
-
-
+            foreach (var item in lines) {
+                
+                var ss =  Regex.Replace(item, @"<(/?)([A-Z][A-Z0-9]*)(.*?)>",//正規表現
+                                                        m=> {
+                                                            return string.Format("<{0}{1}{2}>",
+                                                          m.Groups[1].Value, m.Groups[2].Value.ToLower(), m.Groups[3].Value);
+                                                             }
+                                                        );
+                sb.AppendLine(ss);
+            }
 
             //ファイル出力
             File.WriteAllText(file, sb.ToString());
+
         }
     }
 }
