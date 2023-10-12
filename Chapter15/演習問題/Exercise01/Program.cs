765e289c864db66bf860c9296c6210292d1b0e81
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-10-12 10:32:39 +0900

    Exercise3

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -34,10 +34,29 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_3() {
-            var count = 
+             var count = Library.Books.OrderBy(b=>b.PublishedYear)
+                                      .GroupBy(b => b.PublishedYear);
+            foreach (var books in count) {
+                Console.WriteLine("{0}年  {1}冊",books.Key,books.Count());
+                
+            }     
         }
 
         private static void Exercise1_4() {
+            var books = Library.Books
+                                    .OrderBy(b => b.CategoryId)
+                                    .ThenByDescending(b => b.PublishedYear)
+                                    .Join(Library.Categories, book => book.CategoryId,
+                                                             category => category.Id,
+                                                             (book, category) => new {
+                                                                 Title = book.Title,
+                                                                 Category = category.Name,
+                                                                 Publishedyear = book.PublishedYear
+                                                             }
+                                                             );
+            foreach (var item in books) {
+                Console.WriteLine($"{item.Publishedyear}年,{item.Category},{item.Title}");
+            }
         }
 
         private static void Exercise1_5() {
