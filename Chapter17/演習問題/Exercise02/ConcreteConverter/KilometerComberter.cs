71379700d82aeafef35096bf9731fb15afca08f5
新里 岳大 <OIBC34030@ojs.ac.jp>
2023-10-19 12:04:12 +0900

    17章　Exercise２完了

---
+++ Chapter17/演習問題/Exercise02/ConcreteConverter/KilometerComberter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.ConcreteConverter {
+    public class KilometerComberte : Framework.ConverterBase {
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "キロメーター" || name == UnitName;
+        }
+        protected override double Ratio { get { return 1000; } }
+        public override string UnitName { get { return "キロメーター"; } }
+    }
+}
